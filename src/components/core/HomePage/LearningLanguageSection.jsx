2ee661f4ef6adb4d48687c583360d8737eebfd02
Suryansh Narang <suryanshnarang23@gmail.com>
2024-10-31 12:27:53 +0530

    learninglanguageSection complete

--- src/components/core/HomePage/LearningLanguageSection.jsx
+++ src/components/core/HomePage/LearningLanguageSection.jsx
@@ -3,11 +3,12 @@ import HighlightText from "../HomePage/HighlightText";
 import Know_your_progress from "../../../assets/Images/Know_your_progress.png";
 import compare_with_others from "../../../assets/Images/Compare_with_others.png";
 import plan_your_lessons from "../../../assets/Images/Plan_your_lessons.png";
+import CTAButton from "../HomePage/Button";
 const LearningLanguageSection = () => {
   return (
     <div className="mt-[130px]">
       {/* parent container */}
-      <div className="flex flex-col gap-5">
+      <div className="flex flex-col gap-5 items-center">
         <div className="text-4xl font-semibold text-center ">
           Your Swiss knife for
           <HighlightText text={"learning any language"} />
@@ -19,9 +20,22 @@ const LearningLanguageSection = () => {
         </div>
         {/* div for 3 images */}
         <div className="flex flex-row items-center justify-center mt-5 ">
-          <img src={Know_your_progress} alt="kyp"  className="object-contain -mr-32"/>
-          <img src={compare_with_others} alt="co"  className="object-contain"/>
-          <img src={plan_your_lessons} alt="ppl"  className="object-contain -ml-36"/>
+          <img
+            src={Know_your_progress}
+            alt="kyp"
+            className="object-contain -mr-32"
+          />
+          <img src={compare_with_others} alt="co" className="object-contain" />
+          <img
+            src={plan_your_lessons}
+            alt="ppl"
+            className="object-contain -ml-36"
+          />
+        </div>
+        <div className="w-fit">
+          <CTAButton active={true} linkto={"/signup"}>
+            <div>Learn More</div>
+          </CTAButton>
         </div>
       </div>
     </div>
